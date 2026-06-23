201b06a1a4bf65aed0ae6bd172bed239059e91e2
cheennss234-sys <cheennss234@gmail.com>
2026-06-23 19:26:41 +0800

    Update dictionary.ts

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -20,7 +20,7 @@ const chinaExam: DictionaryResource[] = [
     description: '1',
     category: '中国考试',
     tags: ['大学英语'],
-    url: '/dicts/CET4_T.json',
+    url: '/dicts/zhongshu_guotuotuo_zhuan.json',
     length: 32,
     language: 'en',
     languageCategory: 'en',
